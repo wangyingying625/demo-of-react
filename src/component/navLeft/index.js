eb86eab9032bbfaf6ae4a5d70bca7c9847e074f8
王莹莹 <mldwyy233@gmail.com>
2020-06-11 18:13:23 +0800

    add leftNav

--- src/component/navLeft/index.js
+++ src/component/navLeft/index.js
@@ -1,9 +1,39 @@
 import React from "react";
+import {Menu} from "antd";
+import Icon from '@ant-design/icons';
+import './index.less'
+import MenuConfig from './../../config/menuConfig'
+const SubMenu = Menu.SubMenu
 export default class NavLeft extends React.Component{
+    componentWillMount() {
+        const menuTreeNode = this.renderMeun(MenuConfig)
+        this.setState({
+            menuTreeNode
+        })
+    }
+    // 菜单渲染
+    renderMeun = (data)=>{
+        return data.map((item)=>{
+            if(item.children){
+               return(
+                   <SubMenu title={item.title} key={item.key}>
+                       { this.renderMenu(item.children)}
+                   </SubMenu>
+               )
+            }
+            return <Menu.Item title={item.title} key={item.key}></Menu.Item>
+        })
+    }
     render() {
         return (
             <div>
-                left
+                <div className='logo'>
+                    <img src="/assets/logo-ant.svg" alt='logo'/>
+                    <h1>hello</h1>
+                </div>
+                <Menu theme='dark' >
+                    {this.state.menuTreeNode}
+                </Menu>
             </div>
         );
     }
---
+++ src/config/menuConfig.js
@@ -0,0 +1,13 @@
+export default [
+    {
+        title:'首页',
+        key:'/admin/home'
+    },
+    {
+        title: 'UI',
+        key:'/admin/ui',
+        children:[
+
+        ]
+    }
+]
