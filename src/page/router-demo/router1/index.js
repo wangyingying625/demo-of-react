37126919efa396fc1eed112712e22d5de5eb7997
王莹莹 <mldwyy233@gmail.com>
2020-06-19 09:56:24 +0800

    add router

--- src/page/router-demo/router1/index.js
+++ src/page/router-demo/router1/index.js
@@ -1,10 +1,31 @@
 import React from 'react'
-import {HashRouter,Router,Link} from 'react-router-dom'
-export default class Home extends React.Component{
+// import {Route, BrowserRouter as Router} from 'react-router-dom'
+// import {HashRouter, Router, Link} from 'react-router-dom'
+import { HashRouter, Route, Switch, Redirect,Link} from 'react-router-dom'
+import Main from './main'
+import Topic from './topic'
+
+export default class Home extends React.Component {
     render() {
         return (
             <HashRouter>
-
+                <div>
+                    <ul>
+                        <li>
+                            <Link to='/'>index</Link>
+                        </li>
+                        <li>
+                            <Link to='/main'>main</Link>
+                        </li>
+                        <li>
+                            <Link to='/topic'>topic</Link>
+                        </li>
+                    </ul>
+                    <hr/>
+                    <Route exact path = '/' component={Main}></Route>
+                    <Route path = '/main' component={Main}></Route>
+                    <Route path = '/topic' component={Topic}></Route>
+                </div>
             </HashRouter>
         );
     }
